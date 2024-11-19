5cc26a03c60ce3841b6e0489ea63d8f950b57739
Omolara5861 <debbiegterra@gmail.com>
2024-11-19 09:02:28 +0100

    chore: :label: Define types

---
+++ app/utils/types.ts
@@ -0,0 +1,38 @@
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
+  genres?: Genre[];
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface MovieApiResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MovieDetails extends Movie {
+  runtime: number;
+  tagline: string;
+}
+
+export interface CastMember {
+  id: number;
+  name: string;
+  profile_path: string;
+  character: string;
+}
+
+export interface MovieCredits {
+  cast: CastMember[];
+  crew: CastMember[];
+}
