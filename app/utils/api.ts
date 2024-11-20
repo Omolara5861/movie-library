e7ab35f59fafba7c611a4ab7f3d18c01b4586d3e
omolara5861 <debbiegterra@gmail.com>
2024-11-20 05:26:53 +0100

    chore: :closed_lock_with_key: Add secret key

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,27 +1,37 @@
-import axios from 'axios';
-import { MovieApiResponse, MovieCredits, MovieDetails } from './types/types';
+import axios from "axios";
+import { MovieApiResponse, MovieCredits, MovieDetails } from "./types/types";
 
 // Create Axios instance with base URL and Bearer Token for authorization
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_TMDB_BASE_URL || 'https://api.themoviedb.org/3',
+  baseURL:
+    process.env.NEXT_PUBLIC_TMDB_BASE_URL || "https://api.themoviedb.org/3",
   headers: {
-    accept: 'application/json',
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YmYxMzJlZGY1YzY0NTNmZDEzMmVkNmUyZDY4OTUwZSIsIm5iZiI6MTczMjAyNTMzMi41MDk2MTE2LCJzdWIiOiI2NzNjNTkyMGYwNjM0Y2VhMzgyYjYyNDMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.MQoFQw1QDk4lINm0SRkRrKJcVR7ufhc6lMNAuMhSfBU`, // Add your access token here
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
   },
 });
 
 // Fetch popular movies with proper headers
-export const fetchPopularMovies = (page: number): Promise<{ data: MovieApiResponse }> =>
-  api.get('/movie/popular', { params: { page } });
+export const fetchPopularMovies = (
+  page: number,
+  sortBy: string = "popularity.desc"
+): Promise<{ data: MovieApiResponse }> =>
+  api.get("/discover/movie", {
+    params: { page, sort_by: sortBy },
+  });
 
 // Fetch movie details by ID
-export const fetchMovieDetails = (id: string): Promise<{ data: MovieDetails }> =>
-  api.get(`/movie/${id}`);
+export const fetchMovieDetails = (
+  id: string
+): Promise<{ data: MovieDetails }> => api.get(`/movie/${id}`);
 
 // Fetch movie credits by ID (CASTS)
-export const fetchMovieCredits = (id: string): Promise<{ data: MovieCredits }> =>
-  api.get(`/movie/${id}/credits`);
+export const fetchMovieCredits = (
+  id: string
+): Promise<{ data: MovieCredits }> => api.get(`/movie/${id}/credits`);
 
 // Fetch movie recommendations by ID (Similar movies)
-export const fetchMovieRecommendations = (id: string): Promise<{ data: MovieApiResponse }> =>
+export const fetchMovieRecommendations = (
+  id: string
+): Promise<{ data: MovieApiResponse }> =>
   api.get(`/movie/${id}/recommendations`);
